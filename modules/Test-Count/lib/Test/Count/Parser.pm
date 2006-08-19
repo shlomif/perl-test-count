e83c763b9616a7db0fc05c833a19feb8569ce2af
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-08-19 11:54:43 +0000

    Added the core test counting functionality to the parser.

--- modules/Test-Count/lib/Test/Count/Parser.pm
+++ modules/Test-Count/lib/Test/Count/Parser.pm
@@ -5,6 +5,8 @@ use strict;
 
 use base 'Test::Count::Base';
 
+use Parse::RecDescent;
+
 =head1 NAME
 
 Test::Count::Parser - A Parser for Test::Count. 
@@ -13,10 +15,67 @@ Test::Count::Parser - A Parser for Test::Count.
 
 our $VERSION = '0.01';
 
+sub _get_grammar
+{
+    return <<'EOF';
+update_count: expression            {$thisparser->{count} += $item[1]}
+
+assignments:    assignment ';' assignments
+              | assignment
+
+statement:    assignment
+              | expression               {$item [1]}
+
+assignment:    variable '=' statement   {$thisparser->{vars}->{$item [1]} = $item [3]}
+
+expression:     term '+' expression      {$item [1] + $item [3]}
+              | term '-' expression      {$item [1] - $item [3]}
+              | term
+
+term:           factor '*' term          {$item [1] * $item [3]}
+              | factor '/' term          {$item [1] / $item [3]}
+              | factor
+
+factor:         number
+              | variable                 {$thisparser->{vars}->{$item [1]} || do { die "Undefined variable \"$item[1]\""; } }
+              | '+' factor               {$item [2]}
+              | '-' factor               {$item [2] * -1}
+              | '(' statement ')'        {$item [2]}
+
+number:         /\d+/                    {$item [1]}
+
+variable:       /\$[a-z_]\w*/i
+EOF
+}
+
+sub _calc_parser
+{
+    my $self = shift;
+
+    my $parser = Parse::RecDescent->new($self->_get_grammar());
+
+    $parser->{vars} = {};
+    $parser->{count} = 0;
+
+    return $parser;
+}
+
+sub _parser
+{
+    my $self = shift;
+    if (@_)
+    {
+        $self->{'_parser'} = shift;
+    }
+    return $self->{'_parser'};
+}
+
 sub _init
 {
     my $self = shift;
-    
+
+    $self->_parser($self->_calc_parser());
+
     return 0;
 }
 
@@ -33,6 +92,51 @@ sub _init
     my $value = $parser->get_count();
 
 =head1 FUNCTIONS
+
+=head2 $parser->update_assignments({'text' => $mytext,)
+
+Updates the parser's state based on the assignments in C<$mytext>. For
+example if C<$mytext> is:
+
+     $myvar=500;$another_var=8+$myvar
+
+Then at the end C<$myvar> would be 500 and C<$another_var> would be 508.
+
+=cut
+
+sub update_assignments
+{
+    my ($self, $args) = @_;
+
+    return $self->_parser()->assignments($args->{text});
+}
+
+=head2 $parser->update_count({'text' => $mytext,)
+
+Adds the expression inside C<$mytext> to the internal counter of the
+module. This is in order to count the tests.
+
+=cut
+
+sub update_count
+{
+    my ($self, $args) = @_;
+
+    return $self->_parser()->update_count($args->{text});    
+}
+
+=head2 my $count = $parser->get_count()
+
+Get the total number of tests in the parser.
+
+=cut
+
+sub get_count
+{
+    my $self = shift;
+
+    return $self->_parser()->{count};
+}
 
 =head1 AUTHOR
 
--- modules/Test-Count/t/01-parser.t
+++ modules/Test-Count/t/01-parser.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 use Test::Count::Parser;
 
@@ -11,4 +11,20 @@ use Test::Count::Parser;
     my $parser = Test::Count::Parser->new();
     # TEST
     ok ($parser, "Checking for parser initialization.");
+}
+
+{
+    my $parser = Test::Count::Parser->new();
+    # TEST
+    $parser->update_assignments(
+        {
+            text => q{$NUM_ITERS=5;$TESTS_PER_ITER=7}
+        },
+    );
+    $parser->update_count(
+        {
+            text => q{$NUM_ITERS*$TESTS_PER_ITER}
+        },
+    );
+    is ($parser->get_count(), 35, "Checking for correct calculation");
 }
